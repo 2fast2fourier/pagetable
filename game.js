598e2dc0801baadfa8a035340c963aab34e771da
Matthew Shepard <mattshepard@gmail.com>
2014-12-06 08:49:05 -0700

    Add borders, clean up

--- game.js
+++ game.js
@@ -6,7 +6,42 @@ function loadInit(){
 }
 
 var introData = [
-1,2,3,4,5,6,7,8,9,12,0,0,0,0,0,0,0,0,0,0,0,0,0,11,12,13,14,15,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10
+457,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,443,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+442,  2,  3,  4,  5,  6,  7,  8,  9, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11, 12, 13, 14, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,442,
+456,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,461,444
 ];
 
 var levelData = [
@@ -32,7 +67,7 @@ var SCREEN_WIDTH = 800;
 var SCREEN_HEIGHT = 600;
 var WIDTH = 16;
 var SCREENX = 48;
-var OFFSETX = 6, OFFSETY = 6;
+var OFFSETX = 16, OFFSETY = 12;
 
 function p2x(pos){
     return pos%SCREENX;
@@ -48,6 +83,7 @@ var walls;
 var text;
 var background;
 var arrows;
+var fire;
 
 function create() {
     background = game.add.tileSprite(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 'white32');
@@ -64,7 +100,7 @@ function create() {
     text.setAll('anchor.x', 0.5);
     text.setAll('anchor.y', 0.5);
     text.forEach(function(pt){
-        var pos = pt.z;
+        var pos = pt.z-1;
         if(level[pos] > 0){
             pt.reset(p2x(pos)*WIDTH+WIDTH/2+OFFSETX, p2y(pos)*WIDTH+WIDTH/2+OFFSETY);
             pt.frame = level[pos];
@@ -78,16 +114,6 @@ function create() {
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    // //  Our bullet group
-    // bullets = game.add.group();
-    // bullets.enableBody = true;
-    // bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    // bullets.createMultiple(30, 'bullet');
-    // bullets.setAll('anchor.x', 0.5);
-    // bullets.setAll('anchor.y', 1);
-    // bullets.setAll('outOfBoundsKill', true);
-    // bullets.setAll('checkWorldBounds', true);
-
     walls = game.add.group();
     walls.enableBody = true;
     walls.physicsBodyType = Phaser.Physics.ARCADE;
@@ -100,120 +126,32 @@ function create() {
         wall.reset(pos*16, pos*16);
     });
 
-    // //  The hero!
-
-    // //  The baddies!
-    // aliens = game.add.group();
-    // aliens.enableBody = true;
-    // aliens.physicsBodyType = Phaser.Physics.ARCADE;
-
-    // createAliens();
-
-    // //  The score
-    // scoreString = 'Score : ';
-    // scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
-
-    // //  Lives
-    // lives = game.add.group();
-    // game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
-
-    // //  Text
-    // stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
-    // stateText.anchor.setTo(0.5, 0.5);
-    // stateText.visible = false;
-
-    // for (var i = 0; i < 3; i++) 
-    // {
-    //     var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
-    //     ship.anchor.setTo(0.5, 0.5);
-    //     ship.angle = 90;
-    //     ship.alpha = 0.4;
-    // }
-
-    // //  An explosion pool
-    // explosions = game.add.group();
-    // explosions.createMultiple(30, 'kaboom');
-    // explosions.forEach(setupInvader, this);
-
-    // //  And some controls to play the game with
     arrows = game.input.keyboard.createCursorKeys();
-    // fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    fire = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
 }
 
-// function createAliens () {
-
-//     for (var y = 0; y < 4; y++)
-//     {
-//         for (var x = 0; x < 10; x++)
-//         {
-//             var alien = aliens.create(x * 48, y * 50, 'invader');
-//             alien.anchor.setTo(0.5, 0.5);
-//             alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
-//             alien.play('fly');
-//             alien.body.moves = false;
-//         }
-//     }
-
-//     aliens.x = 100;
-//     aliens.y = 50;
-
-//     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-//     var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-//     //  When the tween loops it calls descend
-//     tween.onLoop.add(descend, this);
-// }
-
-// function setupInvader (invader) {
-
-//     invader.anchor.x = 0.5;
-//     invader.anchor.y = 0.5;
-//     invader.animations.add('kaboom');
-
-// }
-
-// function descend() {
-
-//     aliens.y += 10;
-
-// }
-
 function update() {
 
-    if (player.alive)
-    {
+    if (player.alive){
         player.body.velocity.setTo(0, 0);
 
-        if (arrows.left.isDown)
-        {
+        if (arrows.left.isDown){
             player.body.velocity.x = -200;
         }
-        if (arrows.right.isDown)
-        {
+        if (arrows.right.isDown){
             player.body.velocity.x = 200;
         }
-        if (arrows.up.isDown)
-        {
+        if (arrows.up.isDown){
             player.body.velocity.y = -200;
         }
-        if (arrows.down.isDown)
-        {
+        if (arrows.down.isDown){
             player.body.velocity.y = 200;
         }
+        if (fire.isDown){
+            fire();
+        }
 
-        //  Firing?
-        // if (fireButton.isDown)
-        // {
-        //     fireBullet();
-        // }
-
-        // if (game.time.now > firingTimer)
-        // {
-        //     enemyFires();
-        // }
-
-        //  Run collision
         game.physics.arcade.collide(text, player, collisionHandler);
         game.physics.arcade.collide(walls, player, collisionHandler);
 
@@ -235,146 +173,13 @@ function update() {
 
 function render() {
 
-    // for (var i = 0; i < aliens.length; i++)
-    // {
-    //     game.debug.body(aliens.children[i]);
-    // }
-
 }
 
 function collisionHandler (wall, player) {
-    console.log('coll', arguments);
-    // player.body.velocity.setTo(-player.body.velocity.x, -player.body.velocity.y);
-
-    //  When a bullet hits an alien we kill them both
-    // bullet.kill();
-    // alien.kill();
-
-    // //  Increase the score
-    // score += 20;
-    // scoreText.text = scoreString + score;
-
-    // //  And create an explosion :)
-    // var explosion = explosions.getFirstExists(false);
-    // explosion.reset(alien.body.x, alien.body.y);
-    // explosion.play('kaboom', 30, false, true);
-
-    // if (aliens.countLiving() == 0)
-    // {
-    //     score += 1000;
-    //     scoreText.text = scoreString + score;
-
-    //     enemyBullets.callAll('kill',this);
-    //     stateText.text = " You Won, \n Click to restart";
-    //     stateText.visible = true;
-
-    //     //the "click to restart" handler
-    //     game.input.onTap.addOnce(restart,this);
-    // }
-
-}
-
-function enemyHitsPlayer (player,bullet) {
-    
-    // bullet.kill();
-
-    // live = lives.getFirstAlive();
-
-    // if (live)
-    // {
-    //     live.kill();
-    // }
-
-    // //  And create an explosion :)
-    // var explosion = explosions.getFirstExists(false);
-    // explosion.reset(player.body.x, player.body.y);
-    // explosion.play('kaboom', 30, false, true);
-
-    // // When the player dies
-    // if (lives.countLiving() < 1)
-    // {
-    //     player.kill();
-    //     enemyBullets.callAll('kill');
-
-    //     stateText.text=" GAME OVER \n Click to restart";
-    //     stateText.visible = true;
-
-    //     //the "click to restart" handler
-    //     game.input.onTap.addOnce(restart,this);
-    // }
-
-}
-
-function enemyFires () {
-
-    //  Grab the first bullet we can from the pool
-    // enemyBullet = enemyBullets.getFirstExists(false);
-
-    // livingEnemies.length=0;
-
-    // aliens.forEachAlive(function(alien){
-
-    //     // put every living enemy in an array
-    //     livingEnemies.push(alien);
-    // });
-
-
-    // if (enemyBullet && livingEnemies.length > 0)
-    // {
-        
-    //     var random=game.rnd.integerInRange(0,livingEnemies.length-1);
-
-    //     // randomly select one of them
-    //     var shooter=livingEnemies[random];
-    //     // And fire the bullet from this enemy
-    //     enemyBullet.reset(shooter.body.x, shooter.body.y);
-
-    //     game.physics.arcade.moveToObject(enemyBullet,player,120);
-    //     firingTimer = game.time.now + 2000;
-    // }
-
-}
-
-function fireBullet () {
-
-    //  To avoid them being allowed to fire too fast we set a time limit
-    // if (game.time.now > bulletTime)
-    // {
-    //     //  Grab the first bullet we can from the pool
-    //     bullet = bullets.getFirstExists(false);
-
-    //     if (bullet)
-    //     {
-    //         //  And fire it
-    //         bullet.reset(player.x, player.y + 8);
-    //         bullet.body.velocity.y = -400;
-    //         bulletTime = game.time.now + 200;
-    //     }
-    // }
-
-}
-
-function resetBullet (bullet) {
-
-    //  Called if the bullet goes out of the screen
-    bullet.kill();
 
 }
 
 function restart () {
-
-    //  A new level starts
-    
-    //resets the life count
-    // lives.callAll('revive');
-    //  And brings the aliens back from the dead :)
-    // aliens.removeAll();
-    // createAliens();
-
-    //revives the player
-    // player.revive();
-    //hides the text
-    // stateText.visible = false;
 
 }
 
